9a390bd50821a0522e4788fce5c5b0ddcd7c9937
Devin Bonney <dbonney1@uncc.edu>
2020-04-15 23:44:29 +0000

    Added rudimentary report functionality

--- app/controllers/abuse_reports_controller.rb
+++ app/controllers/abuse_reports_controller.rb
@@ -1,8 +1,11 @@
 class AbuseReportsController < ApplicationController
     def create
-        @article = Article.find(params[:article_id])
+        @article = Article.find(1)
         @abuse_report = @article.abuse_reports.create(abuse_report_params)
         redirect_to article_path(@article)
+    end
+    def index
+        @abuse_reports = AbuseReport.all
     end
     
     private
--- app/models/abuse_report.rb
+++ app/models/abuse_report.rb
@@ -1,3 +1,5 @@
 class AbuseReport < ApplicationRecord
   belongs_to :article
+  validates :reason, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
