d77e2e69fa2e299c9aa810a787b90e3a240fcf17
JackDenkin137 <kcheng12@uncc.edu>
2020-04-15 22:56:55 +0000

    Added tag to articles controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { minimum: 5 }
     validates :text, presence: true, length: { minimum: 10 }
+    validates :tag, presence: true, length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_181511) do
+ActiveRecord::Schema.define(version: 2020_04_15_225122) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_181511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "tag"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -28,6 +29,16 @@ ActiveRecord::Schema.define(version: 2020_04_14_181511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "foo"
+    t.string "bag"
   end
 
   create_table "users", force: :cascade do |t|
