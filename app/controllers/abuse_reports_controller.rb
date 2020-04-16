4dbcbbd0ede8c35cdaca833fa9713f945ec01a8a
Devin Bonney <dbonney1@uncc.edu>
2020-04-16 17:53:55 +0000

    Working on Report Form debugging

--- app/controllers/abuse_reports_controller.rb
+++ app/controllers/abuse_reports_controller.rb
@@ -1,6 +1,6 @@
 class AbuseReportsController < ApplicationController
     def create
-        @article = Article.find(1)
+        @article = Article.find(params[:abuse_report][:article_id])
         @abuse_report = @article.abuse_reports.create(abuse_report_params)
         redirect_to article_path(@article)
     end
@@ -8,9 +8,11 @@ class AbuseReportsController < ApplicationController
         @abuse_reports = AbuseReport.where(article_id: 1)
     end
     def show
-        @abuse_report = AbuseReport.find(params[:article_id])
+        @abuse_report = AbuseReport.find(params[:id])
     end
-    
+    def new
+        @article = Article.find(params[:article_id])
+    end
     private
         def abuse_report_params
             params.require(:abuse_report).permit(:reason, :body)
