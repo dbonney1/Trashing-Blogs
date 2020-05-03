2d70a040e34660845329e17c5889e4c135f64eb9
Devin Bonney <dbonney1@uncc.edu>
2020-05-03 17:19:44 +0000

    Home page now shows 4 most recently created accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_221211) do
+ActiveRecord::Schema.define(version: 2020_05_03_171332) do
 
   create_table "abuse_reports", force: :cascade do |t|
     t.string "reason"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 2020_04_29_221211) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
